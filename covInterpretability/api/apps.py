494fc99800a8fd30803827895b7da8bf11e40c0b
Mariana Suarez <msuarez@id.uff.br>
2022-05-11 19:36:43 -0300

    start using s3

--- covInterpretability/api/apps.py
+++ covInterpretability/api/apps.py
@@ -1,11 +1,45 @@
 import os
+from os.path import exists
 from django.apps import AppConfig
 from django.conf import settings
 from transformers import BertTokenizer, BertForSequenceClassification, BertConfig
 import torch
+import botocore
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
 
+def downloadModels():
+    BUCKET_NAME = 'captum-conecta' 
+    PATH = 'bertimbau3.model' 
+    if(not exists(os.path.join(settings.MODELS, "bertimbau3.model"))):
+        s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+
+        try:
+            print("start downloading")
+            s3.Bucket(BUCKET_NAME).download_file(PATH, 'bertimbau3.model')
+            print("end downloading")
+        except botocore.exceptions.ClientError as e: 
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+    PATH = "bertimbauOthers.model"
+    if(not exists(os.path.join(settings.MODELS, "bertimbauOthers.model"))):
+        s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+
+        try:
+            print("start downloading")
+            s3.Bucket(BUCKET_NAME).download_file(PATH, 'bertimbauOthers.model')
+            print("end downloading")
+        except botocore.exceptions.ClientError as e: 
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
 
 class ApiConfig(AppConfig):
+    downloadModels()
     name = 'api'
     MODEL_FILE = os.path.join(settings.MODELS, "bertimbau3.model")
     modelBert = BertForSequenceClassification.from_pretrained("neuralmind/bert-base-portuguese-cased",
@@ -46,4 +80,5 @@ class ApiConfig(AppConfig):
     6: "Questionável",
     7: "Sem evidências",
     8: "Meia Verdade"
-}
+    }
+
